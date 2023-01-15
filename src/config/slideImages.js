6fb1e6a95aad06b52f469a8e240a395b287272dd
Aravindhkannanb <aravindhkannnan26@gmail.com>
2023-01-15 10:24:55 +0530

    first commit

---
+++ src/config/slideImages.js
@@ -0,0 +1,35 @@
+export const Slides = [
+        {
+            "img": "Images/img1.jpeg"
+        },
+        {
+            "img": "Images/img2.jpeg"
+        },
+        {
+            "img": "Images/img3.jpeg"
+        },
+        {
+            "img": "Images/img4.jpeg"
+        },
+        {
+            "img": "Images/img5.jpeg"
+        },
+        {
+            "img": "Images/img6.jpeg"
+        },
+        {
+            "img": "Images/img7.jpeg"
+        },
+        {
+            "img": "Images/img8.jpeg"
+        },
+        {
+            "img": "Images/img9.jpeg"
+        },
+        {
+            "img": "Images/img10.jpeg"
+        },
+        {
+            "img": "Images/img11.jpeg"
+        },
+]
